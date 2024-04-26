dd0ad561d437ef74e236f136abff705d536fb1d1
lawrencejews <lawrencejews1@gmail.com>
2024-04-26 16:48:48 +0900

    zipkin config added -> TODO

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### SpringBoot-Google-Cloud-Microservices
 - Config-Server on GITHUB: https://github.com/lawrencejews/gcp-microservices-config-server/tree/main
+#### Zipkin
+- Docker command: `docker run -d -p 9411:9411 openzipkin/zipkin`
--- product-service/src/main/java/com/lawrencejews/productservice/entity/Product.java
+++ product-service/src/main/java/com/lawrencejews/productservice/entity/Product.java
@@ -15,10 +15,13 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long productId;
+
     @Column(name = "PRODUCT_NAME")
     private String productName;
+
     @Column(name = "PRODUCT_PRICE")
     private long price;
+
     @Column(name = "PRODUCT_QUANTITY")
     private long quantity;
 }
